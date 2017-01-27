4a81b8945e145bb2a8df8dd3c2d9c8c58562135e
Madhur Toppo <madhurtoppo@Madhurs-MacBook-Pro.local>
2017-01-27 15:52:24 -0500

    Initial Commit

--- FlappyGhost/GameViewController.swift
+++ FlappyGhost/GameViewController.swift
@@ -15,22 +15,28 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
+        
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
+                let view = self.view as! SKView
+                view.showsFPS = true
+                view.showsNodeCount = true
+                
+                view.ignoresSiblingOrder = true
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
+                scene.size = self.view.bounds.size
                 
                 // Present the scene
                 view.presentScene(scene)
+                
+                
+              
             }
             
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        
         }
-    }
+    
 
     override var shouldAutorotate: Bool {
         return true
